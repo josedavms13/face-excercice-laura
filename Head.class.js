28b049d1321dd6c02b8c7301e1044175be2660a8
Laura Esmoris <lesmoris01@gmail.com>
2022-04-07 00:12:32 -0500

    facecita

--- Head.class.js
+++ Head.class.js
@@ -1,12 +1,19 @@
 import {Hair} from "./components/hair/Hair.class.js";
 import {Eye} from "./components/eyes/Eye.class.js";
 import {Mouth} from "./components/mouth/Mouth.class.js";
+import {Ears} from "./components/ears/Ears.class.js";
+
 
 
 export class Head {
-    constructor(id, hairId, leftEyeId, mouthId, amountOfTeeth ) {
+    constructor(id, hairId, leftEyeId, rightEyeId, mouthId, amountOfTeeth, leftClassEar, rightClassEar ) {
+       this.element = document.getElementById(id)
         this.hair = new Hair(hairId);
         this.leftEyeId = new Eye(leftEyeId);
+        this.rightEyeId = new Eye(rightEyeId);
         this.mouth = new Mouth(mouthId, amountOfTeeth);
+        this.rightEar = new Ears(rightClassEar);
+        this.leftEar = new Ears(leftClassEar);
     }
+      
 }
